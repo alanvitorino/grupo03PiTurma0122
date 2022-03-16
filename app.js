d66301eb714d3d9268127a9e1bd15d10c68451f3
Alan Vitorino <alanvitorino@hotmail.com.br>
2022-03-16 12:50:40 -0300

    Scripit base

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+//Criar routes de acordo com projeto.
+// app.get('/', (req, res) => {
+//  res.sendFile(path.join(__dirname, '/views/home.html'))
+// })
+
+// app.get('/cadastro', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/views/cadastro.html'))
+// })
+
+// app.get('/login', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/views/login.html'))
+// })
+
+// app.use(function(req, res, next) {
+//     res.status(404).send("desculpe, essa página não existe");
+// });
+
+app.listen(3000)
